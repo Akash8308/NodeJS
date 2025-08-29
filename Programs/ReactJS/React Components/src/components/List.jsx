74d643e8a131d817bd334628124ef65b60f2efa9
Akash <86683095+Akash8308@users.noreply.github.com>
2025-08-29 20:53:51 +0530

    React Components

---
+++ Programs/ReactJS/React Components/src/components/List.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const myFoodList = (
+  <ul>
+    <li>Chicken & Rice</li>
+    <li>Misal</li>
+    <li>Pav Bhaji</li>
+  </ul>
+);
+
+function List() {
+  return myFoodList;
+}
+
+export default List;
