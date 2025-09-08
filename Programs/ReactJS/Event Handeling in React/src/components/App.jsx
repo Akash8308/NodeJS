f09e29b8aac43cae33071f398090f522b13906b2
Akash <86683095+Akash8308@users.noreply.github.com>
2025-09-09 00:33:45 +0530

    Event Handling in React

---
+++ Programs/ReactJS/Event Handeling in React/src/components/App.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+function App() {
+  const [headingText, setHeadingText] = React.useState("Hello");
+  const [isMouseOver, setMouseOver] = React.useState(false);
+  const [isMouseOut, setMouseOut] = React.useState(true);
+
+  function handleClick() {
+    setHeadingText("Submitted");
+  }
+
+  function handleMouseOver(myVal) {
+    setMouseOver(true);
+  }
+
+  function handleMouseOut() {
+    setMouseOver(false);
+  }
+
+  return (
+    <div className="container">
+      <h1>{headingText}</h1>
+      <input type="text" placeholder="What's your name?" />
+      <button
+        style={{ backgroundColor: isMouseOver ? "Black" : " White" }}
+        onClick={handleClick}
+        onMouseOver={handleMouseOver}
+        onMouseOut={handleMouseOut}
+      >
+        Submit
+      </button>
+    </div>
+  );
+}
+
+export default App;
